7009a754e09ff917533d4630ac4424794d88761f
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2024-05-31 12:40:49 +0300

    fix view code

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	resolve: {
-		alias: {
-			assets: "/src/assets",
-			components: "/src/components",
-		}
-	}
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+      components: "/src/components",
+    }
+  }
 })
