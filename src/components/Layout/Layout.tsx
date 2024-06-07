f81683749240c4391f53adff588b56c5a126f46f
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2024-06-07 15:11:58 +0300

    styled header, add basket

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Drawer, Layout } from "antd";
+import { Badge, Button, Drawer, Layout } from "antd";
 import { Content, Footer, Header } from 'antd/es/layout/layout';
-import { MenuOutlined } from '@ant-design/icons';
+import { MenuOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Typography } from "antd";
 import Sider from 'antd/es/layout/Sider';
 
 interface LayoutProps {
@@ -10,36 +11,65 @@ interface LayoutProps {
 
 export const LayoutPage: React.FC<LayoutProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenBasket, setIsOpenBasket] = useState(false);
 
   const toggleSider = () => {
     setIsOpen(!isOpen);
   };
+  const toggleBasket = () => {
+    setIsOpenBasket(!isOpenBasket);
+  };
 
   return (
     <div className='container'>
-
-    <Layout>
-      <Header style={{background: "#fff", padding: "0px 20px"}}>
-        <Button type="primary" icon={<MenuOutlined />} onClick={toggleSider}>Фильтр</Button>
-      </Header>
       <Layout>
-        <Drawer
-          title="Сайдбар"
-          placement="left"
-          closable={true}
-          onClose={toggleSider}
-          open={isOpen}
+        <Header style={{
+            display: "flex", 
+            flexWrap: "wrap", 
+            gap: "10px", 
+            alignItems: "center", 
+            justifyContent:"space-between", 
+            background: "#fff", 
+            padding: "0px 20px"
+          }}
         >
-          <Sider style={{ background: "#fff", padding: '20px 20px' }} width={200}>
-            Сайдбар
-          </Sider>
-        </Drawer>
-        <Content style={{ background: "#fff", height: "100vh", padding: '20px 25px' }}>
-          {children}
-        </Content>
+          <Typography.Title style={{margin: "0"}}>Ларёк</Typography.Title>
+          <div style={{display: "flex", alignItems: "center", gap: "5px"}}>
+            <Button type="primary" icon={<MenuOutlined />} onClick={toggleSider}>Фильтр</Button>
+            <Badge count={5} >
+              <Button type="primary" icon={<ShoppingCartOutlined />} onClick={toggleBasket} />
+            </Badge>
+          </div>
+        </Header>
+        <Layout>
+          <Drawer
+            title="Фильтры"
+            placement="left"
+            closable={true}
+            onClose={toggleSider}
+            open={isOpen}
+          >
+            <Sider style={{ background: "#fff", padding: '20px 20px' }} width={200}>
+              Фильтров нет
+            </Sider>
+          </Drawer>
+          <Drawer
+            title="Корзина"
+            placement="right"
+            closable={true}
+            onClose={toggleBasket}
+            open={isOpenBasket}
+          >
+            <Sider style={{ background: "#fff", padding: '20px 20px' }} width={200}>
+              Коризна пуста
+            </Sider>
+          </Drawer>
+          <Content style={{ background: "#fff", height: "100vh", padding: '20px 25px' }}>
+            {children}
+          </Content>
+        </Layout>
+        <Footer style={{background: "#fff", padding: "20px 20px"}}>© Организация по производству и продаже товаров</Footer>
       </Layout>
-      <Footer style={{background: "#fff", padding: "20px 20px"}}>Футер</Footer>
-    </Layout>
     </div>
   );
 };
