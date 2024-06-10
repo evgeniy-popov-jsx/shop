020274b4a4c5f4c3a88314ff544555073de1474c
Evgeniy Popov <99990148+JENYAODERZHIMIY@users.noreply.github.com>
2024-06-10 12:51:20 +0300

    add card-skeleton

--- src/components/Card-item/Card-item.tsx
+++ src/components/Card-item/Card-item.tsx
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, Skeleton } from "antd";
 import Meta from 'antd/es/card/Meta';
 
 export const CardItem = () => {
@@ -8,7 +8,12 @@ export const CardItem = () => {
       style={{ width: 240 }}
       cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
     >
-     <Meta title="Название товара" description="Описание товара" />
+      <Skeleton loading={false} active>
+        <Meta
+          title="Название товара"
+          description="Описание товара"
+        />
+      </Skeleton>
     </Card>
   );
 };
