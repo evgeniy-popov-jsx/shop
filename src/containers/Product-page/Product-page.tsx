2d413dbefca2ed8a81075491cf59db4b88de2189
unknown <evgeniy.popov.jsx@gmail.com>
2024-06-13 12:35:02 +0300

    add product-navigate

--- src/containers/Product-page/Product-page.tsx
+++ src/containers/Product-page/Product-page.tsx
@@ -1,23 +1,36 @@
-import { StarFilled } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeftOutlined, StarFilled } from '@ant-design/icons';
 import { Button } from 'antd';
 import { mock } from '../../mock/mock';
 import styles from './product-page.module.css';
 
 export const ProductPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1)
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <img src={mock[0].image} alt={mock[0].title}/>
-        <div className={styles.badget}>{mock[0].price} $</div>
-      </div>
-      <div className={styles.descContainer}>
-        <div className={styles.title}>{mock[0].title}</div>
-        <div className={styles.rating}>
-          <StarFilled className={styles.icon}/> {mock[0].rating.rate} | {mock[0].category}
+      <Button 
+        icon={<ArrowLeftOutlined />}
+        onClick={handleBackClick} 
+      />
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <img src={mock[0].image} alt={mock[0].title}/>
+          <div className={styles.badget}>{mock[0].price} $</div>
         </div>
-        <div className={styles.descriptionTitle}>About the product:</div>
-        <div className={styles.description}>{mock[0].description}</div>
-        <Button>Buy {mock[0].price} $</Button>
+        <div className={styles.descContainer}>
+          <div className={styles.title}>{mock[0].title}</div>
+          <div className={styles.rating}>
+            <StarFilled className={styles.icon}/> {mock[0].rating.rate} | {mock[0].category}
+          </div>
+          <div className={styles.descriptionTitle}>About the product:</div>
+          <div className={styles.description}>{mock[0].description}</div>
+          <Button>Buy {mock[0].price} $</Button>
+        </div>
       </div>
     </div>
   );
