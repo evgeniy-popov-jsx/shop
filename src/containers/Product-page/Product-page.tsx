a11737be8a732ab10d0286ebdf151a7d6f453649
unknown <evgeniy.popov.jsx@gmail.com>
2024-06-13 12:01:41 +0300

    styled product-page

--- src/containers/Product-page/Product-page.tsx
+++ src/containers/Product-page/Product-page.tsx
@@ -1,7 +1,24 @@
+import { StarFilled } from '@ant-design/icons';
+import { Button } from 'antd';
+import { mock } from '../../mock/mock';
 import styles from './product-page.module.css';
 
 export const ProductPage: React.FC = () => {
   return (
-    <>Продукт</>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img src={mock[0].image} alt={mock[0].title}/>
+        <div className={styles.badget}>{mock[0].price} $</div>
+      </div>
+      <div className={styles.descContainer}>
+        <div className={styles.title}>{mock[0].title}</div>
+        <div className={styles.rating}>
+          <StarFilled className={styles.icon}/> {mock[0].rating.rate} | {mock[0].category}
+        </div>
+        <div className={styles.descriptionTitle}>About the product:</div>
+        <div className={styles.description}>{mock[0].description}</div>
+        <Button>Buy {mock[0].price} $</Button>
+      </div>
+    </div>
   );
 };
