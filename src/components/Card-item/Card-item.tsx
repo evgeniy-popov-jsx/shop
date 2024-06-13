d882c1e5fa018902624efc160042509cc26bdfad
unknown <evgeniy.popov.jsx@gmail.com>
2024-06-13 07:29:08 +0300

    add mock, styled card

--- src/components/Card-item/Card-item.tsx
+++ src/components/Card-item/Card-item.tsx
@@ -1,19 +1,29 @@
-import { Card, Skeleton } from "antd";
+import { Card, Skeleton, Rate } from "antd";
 import Meta from 'antd/es/card/Meta';
+import { ItemProps } from 'components/Card-list/Card-list';
+import styles from './card-item.module.css';
+interface CardItemProps {
+    item: ItemProps;
+}
 
-export const CardItem = () => {
+export const CardItem: React.FC<CardItemProps> = ({item}) => {
+  const {image, title, category, price, rating, description} = item;
+
   return (
-    <Card 
-      hoverable
-      style={{ width: 240 }}
-      cover={<img alt="example" src="https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg" width={100} height={300}/>}
-    >
-      <Skeleton loading={false} active>
-        <Meta
-          title="Название товара"
-          description="Описание товара"
-        />
-      </Skeleton>
-    </Card>
+      <Card 
+        hoverable
+        className={styles.card}
+        cover={<img alt="example" src={image} className={styles.img}/>}
+      >
+        <Skeleton loading={false} active>
+          <Meta
+            title={title}
+            description={category}
+          />
+          <div className={styles.description}>{description}</div>
+          <div className={styles.price}>{price} $</div>
+          <Rate className={styles.rate} disabled defaultValue={rating.rate} />
+        </Skeleton>
+      </Card>
   );
 };
--- src/components/Card-list/Card-list.tsx
+++ src/components/Card-list/Card-list.tsx
@@ -1,19 +1,38 @@
 import { CardItem } from 'components/Card-item/Card-item';
+import { mock } from '../../mock/mock.ts';
 import styles from './card-list.module.css';
+import { Link } from 'react-router-dom';
+import { Badge } from 'antd';
+
+export interface ItemProps {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string,
+    rating: {
+      rate: number,
+      count: number
+    }
+}
 
 export const CardList: React.FC = () => {
   return (
     <div className={styles.container}>
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
+      {mock.map((item: ItemProps) => {
+        return (
+          <Link to={`/products/${item.id}`} key={item.id}>
+            {item.rating.rate > 4 
+              ? (
+                <Badge.Ribbon text="Bestseller">
+                  <CardItem item={item}/>
+                </Badge.Ribbon>
+              )
+              : <CardItem item={item}/>}
+          </Link>
+        )
+      })}
     </div>
   );
 };
