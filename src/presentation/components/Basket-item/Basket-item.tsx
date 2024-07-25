f859ba4e169ff59aa89744630b5620cbbc6895ee
unknown <evgeniy.popov.jsx@gmail.com>
2024-07-25 11:46:39 +0300

    remove basket logic

--- src/presentation/components/Basket-item/Basket-item.tsx
+++ src/presentation/components/Basket-item/Basket-item.tsx
@@ -3,12 +3,15 @@ import { Button } from 'antd';
 import { Styled } from './styles';
 import { CloseSquareOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { Product } from 'domain/model/product';
+import { observer } from 'mobx-react-lite';
+import basketStore from 'application/stores/basket-store';
 
 interface ItemProp {
   item: Product
 }
 
-export const BasketItem: React.FC<ItemProp> = ({ item }) => {
+export const BasketItem: React.FC<ItemProp> = observer(({ item }) => {
+  const { removeBusket } = basketStore;
   const [count, setCount] = useState(0);
 
   const { title } = item;
@@ -40,7 +43,8 @@ export const BasketItem: React.FC<ItemProp> = ({ item }) => {
         type="text" 
         size="small" 
         icon={<CloseSquareOutlined />}
+        onClick={()=>removeBusket(item.id)}
       />
     </Styled.Container>
   );
-};
+});
